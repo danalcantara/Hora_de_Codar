3bbe3efca4df8ee48c78fe405d17271afc7a3f6e
Danilo Alcantara <80755693+danalcantara@users.noreply.github.com>
2024-09-11 12:53:02 -0300

    adiciona chamada de codigo

---
+++ hora_de_codar1_kotlin/main.kt
@@ -0,0 +1,12 @@
+fun main(){
+var questoes = HoraDeCodar()
+  println(questoes.ex1())
+  println(questoes.ex2())
+  println(questoes.ex3())
+  println(questoes.ex4())
+  println(questoes.ex5())
+  println(questoes.ex6())
+  println(questoes.ex7())
+  println(questoes.ex8())
+  println(questoes.ex9())
+}
